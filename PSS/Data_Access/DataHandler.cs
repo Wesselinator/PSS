3f8b39eb93f0ffe0101b2cb243684a676a9940b6
USS-Engineer <marco.venter.net@gmail.com>
2021-03-16 14:53:01 +0200

    Creating Data layer structure

---
+++ PSS/Data_Access/DataHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSS.Data_Access
+{
+    class DataHandler
+    {
+        public DataHandler()
+        {
+
+        }
+    }
+}
