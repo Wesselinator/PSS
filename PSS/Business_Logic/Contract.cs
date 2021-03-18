f7f2e8d4a5e747425a678fe66e49283bb0d583d0
Wessel Scholtz <576363@student.belgiumcampus.ac.za>
2021-03-18 11:19:00 +0200

    Added Contract and Service

---
+++ PSS/Business_Logic/Contract.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSS.Business_Logic
+{
+    class Contract
+    {
+        public int ContractID { get; set; }
+        public string ContractName { get; set; }
+        public string ServiceLevel { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public double MonthlyFee { get; set; }
+
+        public Contract(int contractID, string contractName, string serviceLevel, DateTime startDate, DateTime endDate, double monthlyFee)
+        {
+            ContractID = contractID;
+            ContractName = contractName;
+            ServiceLevel = serviceLevel;
+            StartDate = startDate;
+            EndDate = endDate;
+            MonthlyFee = monthlyFee;
+        }
+    }
+}
---
+++ PSS/Business_Logic/Service.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSS.Business_Logic
+{
+    class Service
+    {
+        public int ServiceID { get; set; }
+        public string ServiceName { get; set; }
+        public string ServiceDescription { get; set; }
+
+        public Service(int serviceID, string serviceName, string serviceDescription)
+        {
+            ServiceID = serviceID;
+            ServiceName = serviceName;
+            ServiceDescription = serviceDescription;
+        }
+    }
+}
