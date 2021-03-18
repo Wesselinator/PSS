f6fcba7b9d5abea24ba147015c7fc0791da2d111
Hendrik-19 <576326@student.belgiumcampus.ac.za>
2021-03-18 11:17:44 +0200

    Made client and employee class

---
+++ PSS/Business_Logic/Client.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSS.Business_Logic
+{
+    public class Client : Person
+    {
+        private DateTime registrationDate;
+        private int clientID;
+        //private List<Contact> contracts;
+
+        public DateTime RegistrationDate { get => registrationDate; set => registrationDate = value; }
+        public int ClientID { get => clientID; set => clientID = value; }
+
+        public Client(DateTime registrationDate, int clientID)
+        {
+            this.registrationDate = registrationDate;
+            this.clientID = clientID;
+        }
+
+        public Client() { 
+        
+        }
+    }
+}
---
+++ PSS/Business_Logic/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSS.Business_Logic
+{
+    class Employee
+    {
+        private int employeeID;
+
+        public int EmployeeID { get => employeeID; set => employeeID = value; }
+
+        public Employee(int employeeID)
+        {
+            this.employeeID = employeeID;
+        }
+
+        public Employee()
+        {
+
+        }
+    }
+}
