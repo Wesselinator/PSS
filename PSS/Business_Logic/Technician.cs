ae844cccef4e5a46f5e6a2f5af3351c85b7b7589
Juan-Pierre-Prinsloo <576598@student.belgiumcampus.ac.za>
2021-03-18 11:58:03 +0200

    Added Technician class

---
+++ PSS/Business_Logic/Technician.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSS.Business_Logic
+{
+    class Technician : Employee
+    {
+        private string type;
+
+        private bool isBusy;
+
+        public string Type { get => type; set => type = value; }
+        public bool IsBusy { get => isBusy; set => isBusy = value; }
+
+        public Technician()
+        {
+        }
+
+        public Technician(string type, bool isBusy)
+        {
+            Type = type;
+            IsBusy = isBusy;
+        }
+
+
+        public bool ServiceClient()
+        {
+            return true;
+        }
+    }
+}
