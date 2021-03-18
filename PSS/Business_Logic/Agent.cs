fb23f79a00c6f28abe37428cd7ca87ae7597d343
Juan-Pierre-Prinsloo <576598@student.belgiumcampus.ac.za>
2021-03-18 11:39:47 +0200

    Client child classes created and agent class

---
+++ PSS/Business_Logic/Agent.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSS.Business_Logic
+{
+    class Agent : Employee
+    {
+        private string department; 
+        
+        public string Department { get => department; set => department = value; }
+
+        public Agent()
+        {
+        }
+
+        public Agent(string department)
+        {
+            Department = department;
+        }
+
+        public void AnswerCall()
+        {
+
+        }
+
+        public void RegisterClient()
+        {
+
+        }
+
+        public bool AddClientContract(Client newClient)
+        {
+            return true;
+        }
+    }
+}
