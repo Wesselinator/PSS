2ae7da22790cef26095c50556a4d1f26db0f5ed4
USS-Engineer <marco.venter.net@gmail.com>
2021-04-09 16:37:14 +0200

    Started Client Maintenance form

--- PSS/Presentation_Layer/ClientMaintenance.cs
+++ PSS/Presentation_Layer/ClientMaintenance.cs
@@ -7,14 +7,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using PSS.Business_Logic;
 
 namespace PSS.Presentation_Layer
 {
     public partial class ClientMaintenance : Form
     {
+        Client client;
+
         public ClientMaintenance()
         {
             InitializeComponent();
+        }
+
+        public void ReceiveClient(Client clientToUpdate)
+        {
+            //update form to register components/name
+            lblTask.Text = "Update Client";
+
+            client = new IndividualClient();
+            client = clientToUpdate;
+            if (clientToUpdate is IndividualClient)
+            {
+                lblBusinessName.Hide();
+                txtBusinessName.Hide();
+            } else
+            {
+                client = new BusinessClient();
+                client = clientToUpdate;
+                lblBusinessName.Show();
+                txtBusinessName.Show();
+                //txtBusinessName.Text = client.BusinessName;
+            }
+           
+            
         }
     }
 }
