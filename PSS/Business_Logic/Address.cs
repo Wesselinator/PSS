6fb5448e1bc698107b6371ffb505e19a9d68b06e
Wessel Scholtz <576363@student.belgiumcampus.ac.za>
2021-04-16 14:51:55 +0200

    Added Adress

---
+++ PSS/Business_Logic/Address.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace PSS.Business_Logic
+{
+    public class Address
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public string Province { get; set; }
+
+        public Address(string street, string city, string postalCode, string province)
+        {
+            this.Street = street;
+            this.City = city;
+            this.PostalCode = postalCode;
+            this.Province = province;
+        }
+
+        public Address(DataRow row)
+        {
+            this.Street = row.Field<string>("Street");
+            this.City = row.Field<string>("City");
+            this.PostalCode = row.Field<string>("PostalCode");
+            this.Province = row.Field<string>("Province");
+        }
+
+        public Address()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            //TODO: Create the string!
+            return base.ToString();
+        }
+    }
+}
--- PSS/Business_Logic/BusinessClient.cs
+++ PSS/Business_Logic/BusinessClient.cs
@@ -4,39 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data;
+using PSS.Data_Access;
 
 namespace PSS.Business_Logic
 {
+    //TODO: inherit from BusinessClientPerson
     class BusinessClient : Client
     {
-        private string businessName;
-
-        public string BusinessName { get => businessName; set => businessName = value; }
-        public string ContactPersoneName { get => FirstName; set => FirstName = value; }
-        public string ContactPersonSurname { get => LastName; set => LastName = value; }
+        public string BusinessName { get; set; }
+        public Address Adress { get; set; }
         
-        public BusinessClient(int businessID, DateTime registrationDate, string businessName, string contactPersoneName, string contactPersonSurname, string cellphoneNumber, string telephoneNumber, string email, string streetAddress, string cityAddress, string postalCode, string province) : base(businessID, registrationDate, contactPersoneName, contactPersonSurname, cellphoneNumber, telephoneNumber, email, streetAddress, cityAddress, postalCode, province)
+        //TODO: Build Adress
+        public BusinessClient(int businessID, string businessName, string contactPersoneName, string contactPersonSurname, string cellphoneNumber, string telephoneNumber, string email, string streetAddress, string cityAddress, string postalCode, string province) : base(businessID, contactPersoneName, contactPersonSurname, cellphoneNumber, telephoneNumber, email)
         {
             BusinessName = businessName;
-            ContactPersoneName = contactPersoneName;
-            ContactPersonSurname = contactPersonSurname;
         }
-
-        public BusinessClient(int businessID, string businessName, string contactPersoneName, string contactPersonSurname, string cellphoneNumber, string telephoneNumber, string email, string streetAddress, string cityAddress, string postalCode, string province) : base(businessID, contactPersoneName, contactPersonSurname, cellphoneNumber, telephoneNumber, email, streetAddress, cityAddress, postalCode, province)
-        {
-            BusinessName = businessName;
-            ContactPersoneName = contactPersoneName;
-            ContactPersonSurname = contactPersonSurname;
-        }
-
         public BusinessClient()
         {
 
         }
 
-        public BusinessClient(DataRow row)
+        public BusinessClient(DataRow row) : base(row)
         {
-
+            BusinessName = row.Field<string>("BusinessName");
+            Adress = new Address(DataEngine.GetByID("Address", "AddressID", row.Field<int>("AddressID")));
         }
     }
 }
