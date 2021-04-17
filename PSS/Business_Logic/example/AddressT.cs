2c3fc20757071e43bfd973685efbb7bb802c476c
Wessel Scholtz <576363@student.belgiumcampus.ac.za>
2021-04-17 21:54:53 +0200

    The 2 Ways of doing this

---
+++ PSS/Business_Logic/example/AddressT.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using PSS.Data_Access;
+
+namespace PSS.Business_Logic.example
+{
+    public class AddressT : IModifyable
+    {
+        public int AddressID { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public string Province { get; set; }
+
+        private static readonly string TableName = "Address";
+        private static readonly string IDColumn = "AddressID";
+
+        private static readonly TableEngine AddressTable = new TableEngine(TableName, IDColumn);
+
+        public AddressT()
+        { }
+        public AddressT(int addressID, string street, string city, string postalCode, string province) : this()
+        {
+            this.AddressID = addressID;
+            this.Street = street;
+            this.City = city;
+            this.PostalCode = postalCode;
+            this.Province = province;
+        }
+        public AddressT(string street, string city, string postalCode, string province)
+                : this(AddressTable.GetNextID(), street, city, postalCode, province)
+        { }
+
+        #region DataBase
+
+        public AddressT(DataRow row) : this()
+        {
+            this.AddressID = row.Field<int>(IDColumn);
+            this.Street = row.Field<string>("Street");
+            this.City = row.Field<string>("City");
+            this.PostalCode = row.Field<string>("PostalCode");
+            this.Province = row.Field<string>("Province");
+        }
+
+        //P3
+        public AddressT(int ID)
+                : this(AddressTable.GetByID(ID))
+        { }
+
+        //P4
+        public void Save()
+        {
+            AddressTable.UpdateORInsert(this, AddressID);
+        }
+
+        string IUpdateable.Update()
+        {
+            StringBuilder sql = new StringBuilder();
+            sql.AppendLine("UPDATE " + TableName);
+
+            sql.Append("SET ");
+            sql.Append("Street = '"+ Street +"', ");
+            sql.Append("City = '" + City + "', ");
+            sql.Append("PostalCode = '" + PostalCode + "', ");
+            sql.AppendLine("Province = '" + Province + "'");
+
+            sql.AppendLine("WHERE " + IDColumn + " = " + AddressID);
+
+            return sql.ToString();
+        }
+
+        string IInsertable.Insert()
+        {
+            StringBuilder sql = new StringBuilder();
+            sql.AppendLine("INSERT INTO " + TableName);
+
+            sql.Append("VALUES (");
+            sql.Append(AddressID +", ");
+            sql.Append("'" + Street + "', ");
+            sql.Append("'" + City + "', ");
+            sql.Append("'" + PostalCode + "', ");
+            sql.Append("'" + Province + "' ");
+            sql.AppendLine(");");
+
+            return sql.ToString();
+        }
+
+        #endregion
+
+
+        public string AdressString()
+        {
+            return string.Format("{0}, {1}, {2}", Street, City, Province);
+        }
+
+
+        public override string ToString()
+        {
+            return string.Format("AddressID: {0} | Street: {1} | City: {2} | Postal Code: {3} | Province: {4}", AddressID, Street, City, PostalCode, Province);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Address address &&
+                   Street == address.Street &&
+                   City == address.City &&
+                   PostalCode == address.PostalCode &&
+                   Province == address.Province;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = -1008569148;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Street);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(City);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(PostalCode);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Province);
+            return hashCode;
+        }
+    }
+}
---
+++ PSS/Data_Access/TableEngine.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Data;
+
+namespace PSS.Data_Access
+{
+    public class TableEngine
+    {
+        public string TableName { get; private set; }
+        public string[] IDColumns { get; private set; }
+        public string IDColumn { get => IDColumns[0]; private set => IDColumns = new string[] { value }; } //IMPORTANT: setting IDcolumn recreates IDColums[]!
+        private bool IsSingle { get => IDColumns.Length == 1; }
+
+        public TableEngine(string tableName, params string[] idColumns)
+        {
+            this.TableName = tableName;
+            this.IDColumns = idColumns;
+        }
+
+        public TableEngine(string tableName, string idColumn)// : this(tableName, new string[] { idColumn })
+        {
+            this.TableName = tableName;
+            this.IDColumn = idColumn; //test
+        }
+
+        public int GetNextID()
+        {
+            if (!IsSingle)
+            {
+                throw new Exception(); //TODO: Nice Exception
+            }
+
+            //VERBOSE: Becuase I want to see everything that happens
+            string sql = string.Format("SELECT * FROM {0} ORDER BY {1} DESC", TableName, IDColumn);
+            DataTable dt = DataHandler.getDataTable(sql);
+            DataRow dr = dt.Rows[0];
+            int nextID = dr.Field<int>(IDColumn) + 1;
+            return nextID;
+        }
+
+        #region GetByID
+
+        public DataRow GetByID(params int[] IDs)
+        {
+            //exists
+            string sql = string.Format("SELECT * FROM {0} ", TableName);
+
+            if (IsSingle)
+            {
+                sql += WhereID(IDs[0]);
+            }
+            else
+            {
+                if (IDs.Length != IDColumns.Length) // IDs entered do not coraspond to Columns
+                {
+                    throw new Exception(); //TODO: Nice Exception
+                }
+
+                sql += WhereIDs(IDs);
+            }
+
+
+            DataTable dt = DataHandler.getDataTable(sql);
+            if (dt.Rows.Count == 0)
+            {
+                throw new Exception(); //TODO: Throw Exception
+            }
+            return dt.Rows[0];
+        }
+
+        //eventually move to SQLstring class
+        public string WhereID(int id)
+        {
+            return string.Format("WHERE {0} = {1}", IDColumn, id);
+        }
+
+        private string WhereIDs(int[] ids) 
+        {
+            string[] wheres = new string[IDColumns.Length];
+            for (int i = 0; i < IDColumns.Length; i++)
+            {
+                wheres[i] = string.Format("{0} = {1}", IDColumns[i], ids[i]);
+            }
+            return "WHERE " + string.Join(" AND ", wheres);
+        }
+
+        #endregion
+
+
+        public void UpdateORInsert(IModifyable data, int ID)
+        {
+
+        }
+    }
+}
