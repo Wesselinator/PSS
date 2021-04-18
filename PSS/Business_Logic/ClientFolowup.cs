d811eb0f068723cd19521757da9ff6f01939063e
Wessel Scholtz <576363@student.belgiumcampus.ac.za>
2021-04-18 19:48:45 +0200

    Client Followup added

---
+++ PSS/Business_Logic/ClientFolowup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSS.Business_Logic
+{
+    class ClientFolowup : BaseSingleID
+    {
+        private Person p;
+        public int ClientFolowupID { get => ID; private set => ID = value; }
+        public int BusinessClientID { get; private set; }
+        public FollowUp FollowUpReport { get; private set; } //pribatte?
+        public DateTime FollowUpDate { get; set; }
+
+
+    }
+}
